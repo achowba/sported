1aac8759de851b2629014e884c80447896caa4f9
Achowba <princeatauba@gmail.com>
2018-01-17 22:03:20 +0100

    setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import secret from './config';
+
+class App extends Component {
+
+  	render() {
+		return (
+			<div>
+				Sported
+			</div>
+		);
+  	}
+}
+
+export default App;
---
+++ src/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    API_KEY: process.env.REACT_APP_API_KEY
+}
